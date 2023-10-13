0962f942ca78ea3877d72bc6dd127a0cc3fbbc26
Ayush Kumar Singh <53579697+ayushcloudops@users.noreply.github.com>
2023-10-13 17:22:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,41 @@
 module agent {
-    source      =   "./module/ec2-cw-agent/"
+    source      =   "./ec2-cw-agent/"
     instance_id =   var.instance_id
     requests    =   var.requests
 }
 
 module ec2-CloudWatch-cpu   {
-    source  =   "./module/ec2-cw/cpu"
+    source  =   "./ec2-cw/cpu"
     instance_id =   var.instance_id
 }
 
 module ec2-CloudWatch-disk   {
-    source  =   "./module/ec2-cw/disk"
+    source  =   "./ec2-cw/disk"
     instance_id =   var.instance_id
 }
 
 module ec2-CloudWatch-status   {
-    source  =   "./module/ec2-cw/status"
+    source  =   "./ec2-cw/status"
     instance_id =   var.instance_id
 }
 
 module ec2-CloudWatch-memory   {
-    source  =   "./module/ec2-cw/memory"
+    source  =   "./ec2-cw/memory"
     instance_id =   var.instance_id
 }
 
 module lb_request {
-    source        =   "./module/load-balancer/requestCount/"
+    source        =   "./load-balancer/requestCount/"
     loadbalancers = var.loadbalancers
 }
 
 module lb_target_response_time {
-    source        =   "./module/load-balancer/targetresponsetime/"
+    source        =   "./load-balancer/targetresponsetime/"
     loadbalancers = var.loadbalancers
 }
 
 module unhealthyhost {
-    source        =   "./module/load-balancer/unhealthyHost/"
+    source        =   "./load-balancer/unhealthyHost/"
     loadbalancers = var.loadbalancers
 }
 
